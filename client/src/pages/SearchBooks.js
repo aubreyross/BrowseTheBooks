31feaaa4a8ff241d1690c53e626922398b79557f
aubreyross <aubrey.ross04@gmail.com>
2022-02-28 16:13:02 -0600

    defined bookInput in pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -56,7 +56,7 @@ const SearchBooks = () => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const bookInput = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -75,7 +75,7 @@ const SearchBooks = () => {
       }
 
       // if book successfully saves to user's account, save book id to state
-        setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+        setSavedBookIds([...savedBookIds, bookInput.bookId]);
         } catch (err) {
         console.error(err);
     }
